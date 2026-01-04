e44232811ff42161d35b85eff8860a85ba7b4354
MD Faizan <faizanrock705@gmail.com>
2026-01-04 23:09:19 +0530

    Allow unauthenticated access to create-admin endpoint

--- skillsync-backend/apps/users/views.py
+++ skillsync-backend/apps/users/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate, get_user_model
 
+
 from .serializers import (
     RegisterSerializer,
     LoginSerializer,
@@ -105,9 +106,12 @@ class UserStatsView(generics.GenericAPIView):
 # TEMPORARY: CREATE ADMIN USER (PRODUCTION ONLY)
 # =========================================================
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+
 
 @api_view(["POST"])
+@permission_classes([AllowAny])
 def create_admin(request):
     email = "admin@skillsync.dev"
 
